de2e56278a91541ce6c5513f1e48d9a6076b1c3e
tekostyshyn <tekostyshyn@gmail.com>
2023-02-05 19:59:49 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+};
+
+formEl.addEventListener("submit", handleSubmit);
