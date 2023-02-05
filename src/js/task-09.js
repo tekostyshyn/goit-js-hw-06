b81e6dc3eeb6207b290bf19f39cf5b842dc10720
tekostyshyn <tekostyshyn@gmail.com>
2023-02-05 20:07:03 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const colorLabelEl = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+const onClickChangeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorLabelEl.textContent = document.body.style.backgroundColor;
+};
+
+button.addEventListener("click", onClickChangeColor);
