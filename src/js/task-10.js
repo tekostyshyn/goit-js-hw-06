fd96b4070d00b9e25a02691c1b096e0c839069bd
tekostyshyn <tekostyshyn@gmail.com>
2023-02-05 21:48:03 +0200

    task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+const createBoxes = (amount) => {
+  amount = input.value;
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement('div');
+    newBox.style.width = 30 + i * 10 + 'px';
+    newBox.style.height = 30 + i * 10 + 'px';
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxes.push(newBox);
+  }
+  boxesContainer.append(...boxes);
+};
+
+const destroyBoxes = () => {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
